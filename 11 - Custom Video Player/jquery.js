49f5ce632c8c298c007796218c795e23516f78f0
HawkingHuang <math0915@gmail.com>
2024-02-22 16:02:45 +0800

    Finish unit 15

---
+++ 11 - Custom Video Player/jquery.js
@@ -0,0 +1,8 @@
+const player = $(".player");
+const video = $(".player .viewer");
+const progress = $(".player .progress");
+const progressBar = $(".player .progress__filled");
+const toggle = $(".player .toggle");
+const skipButtons = $(".player[data-skip]");
+const ranges = $(".player .player__slider");
+console.log(progress);
